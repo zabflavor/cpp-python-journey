484156cd03734ab4c38eb8ee5333b8454e58046a
zabflavor <2026302114243@whu.edu.cn>
2026-09-22 22:54:19 +0800

    Rename 变量.cpp to day02/变量.cpp

---
+++ day02/变量.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//单行注释
+	/*变量(给一段存储空间起名，方便操作这段内存）创建的方法：数据类型  变量名=变量的初始值*/
+	int a = 10;
+	cout << "a=" << a << endl;
+	system("pause");
+    return 0;
+}
