0d4e1a8dab7820d4e580ce3530193f3f2cce5746
zabflavor <2026302114243@whu.edu.cn>
2026-09-24 21:09:00 +0800

    Rename 布尔类型.cpp to day04/布尔类型.cpp

---
+++ day04/布尔类型.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//创建bool数据类型
+	bool flag = true;//true代表真(只要是非零的值都代表真)
+	cout << flag << endl;
+	flag = false;//false代表假
+	cout << flag << endl;
+	//本质上 1代表真 0代表假
+	system("pause");
+	return 0;
+
+}
