5b19edf2090990d47f18676f46f771e160a0a6cb
zabflavor <2026302114243@whu.edu.cn>
2026-09-24 21:09:19 +0800

    Rename 数据的输入.cpp to day04/数据的输入.cpp

---
+++ day04/数据的输入.cpp
@@ -0,0 +1,13 @@
+//数据的输入：用于从键盘上获取数据
+//cin>>变量
+#include<iostream>
+using namespace std;
+int main() {
+	//浮点型
+	float f = 3.14;
+	cout << "请给浮点型变量f赋值：" << endl;
+	cin >> f;
+	cout << "浮点型变量f=" << f<< endl;
+    system("pause");
+	return 0;
+}
